63a35419ad7df4b32c879d45b0f0db21f4c35c94
Dario Baldini <baldini.dario98@gmail.com>
2023-04-04 18:28:52 +0200

    test js media query

--- tesi_db_script.js
+++ tesi_db_script.js
@@ -3,8 +3,12 @@ var nav = false;
 
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 
-function open_sidebar() {
-  document.getElementById("meta_sidebar").style.width = "40%";
+function open_sidebar(x) {
+  if (x.matches) { // If media query matches
+    document.getElementById("meta_sidebar").style.width = "95%";
+  } else {
+    document.getElementById("meta_sidebar").style.width = "40%";
+  }
   nav = true;
 }
 
@@ -16,5 +20,9 @@ function close_sidebar() {
 }
 
 function toggle_sidebar() {
-  nav ? close_sidebar() : open_sidebar();
+  nav ? close_sidebar() : open_sidebar(x);
 }
+
+var x = window.matchMedia("(max-width: 480px)")
+open_sidebar(x) // Call listener function at run time
+x.addListener(open_sidebar) // Attach listener function on state changes
