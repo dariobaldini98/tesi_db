2b2899adcd5cc8d5e86143957ca50e3c2a285984
Dario Baldini <baldini.dario98@gmail.com>
2023-04-04 17:37:51 +0200

    change some sidebar style

--- tesi_db_script.js
+++ tesi_db_script.js
@@ -2,16 +2,19 @@
 /* sidebar */
 
 var nav = false;
+
 function open_sidebar() {
   document.getElementById("meta_sidebar").style.width = "40%";
   document.getElementById("content").style.marginLeft = "40%";
   nav = true;
 }
+
 function close_sidebar() {
   document.getElementById("meta_sidebar").style.width = "0";
   document.getElementById("content").style.marginLeft = "0";
   nav = false;
 }
+
 function toggle_sidebar() {
   nav ? close_sidebar() : open_sidebar();
 }
