8fb6ee616df8b9d81682b734d294e6356cbd35d8
Dario Baldini <baldini.dario98@gmail.com>
2023-08-05 12:35:27 +0200

    test fixing addEventListener

--- tesi_db_script.js
+++ tesi_db_script.js
@@ -3,8 +3,10 @@ const sidebar = document.querySelectorAll(".sidebar");
 const sidebar_button_open = document.getElementById("sidebar_button_open");
 const sidebar_button_close = document.getElementById("sidebar_button_close");
 
-sidebar_button_open.addEventListener("click", open_sidebar);
-sidebar_button_close.addEventListener("click", close_sidebar);
+window.onload = () => {
+  sidebar_button_open.addEventListener("click", open_sidebar);
+  sidebar_button_close.addEventListener("click", close_sidebar);
+}
 
 function open_sidebar() {
   if (window.matchMedia("(max-width: 480px)").matches) {
