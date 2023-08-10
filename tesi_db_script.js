80f0eb14a6b20143b5726267483a0cca9c47f789
Dario Baldini <baldini.dario98@gmail.com>
2023-08-10 20:53:57 +0200

    add settings sidebar script

--- tesi_db_script.js
+++ tesi_db_script.js
@@ -3,9 +3,16 @@ const meta_sidebar = document.getElementById("meta_sidebar");
 const open_meta_sidebar = document.getElementById("open_meta_sidebar");
 const close_meta_sidebar = document.getElementById("close_meta_sidebar");
 
+const settings_sidebar = document.getElementById("settings_sidebar");
+const open_settings_sidebar = document.getElementById("open_settings_sidebar");
+const close_settings_sidebar = document.getElementById("close_settings_sidebar");
+
+
 window.onload = () => {
   open_meta_sidebar.addEventListener("click", expand_meta_sidebar);
   close_meta_sidebar.addEventListener("click", collapse_meta_sidebar);
+  open_settings_sidebar.addEventListener("click", expand_settings_sidebar);
+  close_settings_sidebar.addEventListener("click", collapse_settings_sidebar);
 }
 
 function expand_meta_sidebar() {
@@ -18,4 +25,16 @@ function expand_meta_sidebar() {
 
 function collapse_meta_sidebar() {
   meta_sidebar.style.width = "0";
+}
+
+function expand_settings_sidebar() {
+  if (window.matchMedia("(max-width: 480px)").matches) {
+    settings_sidebar.style.width = "95%";
+  } else {
+    settings_sidebar.style.width = "40%";
+  };
+}
+
+function collapse_settings_sidebar() {
+  settings_sidebar.style.width = "0";
 }
